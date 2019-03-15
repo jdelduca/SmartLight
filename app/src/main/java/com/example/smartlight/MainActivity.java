4bd57aba63cf254efd5fa0793e05a47a3ca708c3
jdelduca <36080254+jdelduca@users.noreply.github.com>
2019-03-14 20:48:23 -0400

    adding dimmers

--- app/src/main/java/com/example/smartlight/MainActivity.java
+++ app/src/main/java/com/example/smartlight/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.smartlight;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // test
+
+        ImageView iv = (ImageView)findViewById(R.id.headerLogo);
+        iv.setImageResource(R.drawable.logo);
     }
 }
